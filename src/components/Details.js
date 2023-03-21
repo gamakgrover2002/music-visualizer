f886644c35e2fdcd2cf24bf425d9661dc4e8d052
Gamak Grover <112484328+gamakgrover2002@users.noreply.github.com>
2023-03-21 17:58:41 +0530

    Add files via upload

--- src/components/Details.js
+++ src/components/Details.js
@@ -4,7 +4,7 @@ const Details = () => {
   return (
     <div id="content">
     <h2>Music Visualizer</h2>
-    <h5>Click on resume to begin song and on suspend to pause it</h5>
+    <h5>Click on start to begin song and on stop to pause it</h5>
     </div>
   )
 }
