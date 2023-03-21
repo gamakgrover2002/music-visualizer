417eee639354bbde65ce40b01d2956aa942faa59
Gamak Grover <112484328+gamakgrover2002@users.noreply.github.com>
2023-03-21 17:56:32 +0530

    Add files via upload

---
+++ Details.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Details = () => {
+  return (
+    <div id="content">
+    <h2>Music Visualizer</h2>
+    <h5>Click on start to begin song and on stop to pause it</h5>
+    </div>
+  )
+}
+
+export default Details;
