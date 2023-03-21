aa8bf5750eac57e61cc393a09fa19918a3e21ee3
Gamak Grover <112484328+gamakgrover2002@users.noreply.github.com>
2023-03-21 21:57:35 +0530

    Update Details.js

--- src/components/Details.js
+++ src/components/Details.js
@@ -4,7 +4,7 @@ const Details = () => {
   return (
     <div id="content">
     <h2>Music Visualizer</h2>
-    <h5>Click on start to begin song and on stop to pause it</h5>
+    <h5>Click on start to resume song and on stop to pause it</h5>
     </div>
   )
 }
